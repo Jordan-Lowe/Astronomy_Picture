e92666e418978ecc641fcb9d75b7454c62bc06cd
Jordan Lowe <jordanlowe659@gmail.com>
2023-09-23 17:03:20 +1200

    Set up basic stuff before changing to server side rendering

--- src/main.ts
+++ src/main.ts
@@ -1,100 +1,168 @@
-// My Interface for the data fetched from the NASA API
+import { initializeApp } from 'firebase/app'
+import {
+  getFirestore,
+  collection,
+  getDoc,
+  addDoc,
+  doc,
+  updateDoc,
+  increment,
+  setDoc,
+} from 'firebase/firestore'
+
 interface Data {
-	error?: { message: string };
-	code?: any;
-	msg?: string;
-	media_type?: "image" | "video";
-	url?: string;
-	title?: string;
-	date?: string;
-	copyright?: string;
-	explanation?: string;
+  error?: { message: string }
+  code?: any
+  msg?: string
+  media_type?: 'image' | 'video'
+  url?: string
+  title?: string
+  date?: string
+  copyright?: string
+  explanation?: string
 }
 
 class Cards {
-	private photos: Data[] = [];
-	private cardState: string = "photos";
+  private photos: Data[] = []
+  private cardState: string = 'photos'
 
-	constructor(public onImageClick: (imageData: Data) => void) {
-		this.fetchData();
-	}
+  constructor(public onImageClick: (imageData: Data) => void) {
+    this.fetchData()
+  }
+  private async storeDataInFirebase(data: Data) {
+    if (data.date) {
+      try {
+        const docRef = doc(db, 'nasaData', data.date)
+        const docSnap = await getDoc(docRef)
 
-	private fetchData() {
-		// Fetch directly from NASA API for 9 images
-		fetch(
-			"https://api.nasa.gov/planetary/apod?api_key=E1RwjszVbe0bxmJHHZ5mUr8uDuTpKUYPiHkTVosB&count=9"
-		)
-			.then((response) => response.json())
-			.then((data) => {
-				this.photos = data;
-				this.render();
-			})
-			.catch((error) => console.error("Error fetching photos:", error));
-	}
+        if (!docSnap.exists()) {
+          // If document doesn't exist, create it with clicks set to 0
+          await setDoc(docRef, {
+            ...data,
+            clicks: 0,
+          })
+        }
+        // If the document already exists, we don't need to do anything here since the clicks will be updated via the incrementClickCount method
+      } catch (error) {
+        console.error('Error checking or adding document:', error)
+      }
+    }
+  }
 
-	public render() {
-		const cardWrapper = document.createElement("div");
-		cardWrapper.className = "cardWrapper";
-		cardWrapper.setAttribute("data-state", this.cardState);
+  private async incrementClickCount(data: Data) {
+    if (data.date) {
+      try {
+        const docRef = doc(db, 'nasaData', data.date)
+        const docSnap = await getDoc(docRef)
 
-		const uiLayer = document.createElement("div");
-		uiLayer.className = "ui-layer";
+        if (docSnap.exists()) {
+          // If document exists, update the clicks
+          await updateDoc(docRef, {
+            clicks: increment(1),
+          })
+        } else {
+          // If document doesn't exist, create it with clicks set to 1
+          await setDoc(docRef, {
+            ...data,
+            clicks: 1,
+          })
+        }
+      } catch (error) {
+        console.error('Error updating click count:', error)
+      }
+    }
+  }
 
-		this.photos.forEach((photoData) => {
-			const uiPhoto = document.createElement("div");
-			uiPhoto.className = "ui-photo";
-			uiPhoto.setAttribute("data-photo", photoData.url!);
-			uiPhoto.addEventListener("click", () => this.onImageClick(photoData));
+  private fetchData() {
+    fetch(
+      'https://api.nasa.gov/planetary/apod?api_key=E1RwjszVbe0bxmJHHZ5mUr8uDuTpKUYPiHkTVosB&count=2'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.photos = data
+        this.photos.forEach((photo) => {
+          this.storeDataInFirebase(photo)
+        })
+        this.render()
+      })
+      .catch((error) => console.error('Error fetching photos:', error))
+  }
 
-			const img = document.createElement("img");
-			img.src = photoData.url!;
-			img.alt = photoData.title!;
+  public render() {
+    const cardWrapper = document.createElement('div')
+    cardWrapper.className = 'cardWrapper'
+    cardWrapper.setAttribute('data-state', this.cardState)
 
-			uiPhoto.appendChild(img);
-			uiLayer.appendChild(uiPhoto);
-		});
+    const uiLayer = document.createElement('div')
+    uiLayer.className = 'ui-layer'
 
-		cardWrapper.appendChild(uiLayer);
+    this.photos.forEach((photoData) => {
+      const uiPhoto = document.createElement('div')
+      uiPhoto.className = 'ui-photo'
+      uiPhoto.setAttribute('data-photo', photoData.url!)
+      uiPhoto.addEventListener('click', () => {
+        this.onImageClick(photoData)
+        this.incrementClickCount(photoData)
+      })
 
-		const container = document.querySelector("#leftContainer");
-		container?.appendChild(cardWrapper);
-	}
+      const img = document.createElement('img')
+      img.src = photoData.url!
+      img.alt = photoData.title!
+
+      uiPhoto.appendChild(img)
+      uiLayer.appendChild(uiPhoto)
+    })
+
+    cardWrapper.appendChild(uiLayer)
+
+    const container = document.querySelector('#leftContainer')
+    container?.appendChild(cardWrapper)
+  }
 }
 
 function handleImageClick(imageData: Data): void {
-	// Set the dailyImage to the image that was clicked
-	document.getElementById("dailyImage")!.setAttribute("src", imageData.url!);
-	document.getElementById("imageTitle")!.textContent = imageData.title!;
-	document.getElementById("imageDate")!.textContent = imageData.date!;
-	document.getElementById("imageDescription")!.textContent =
-		imageData.explanation!;
+  document.getElementById('dailyImage')!.setAttribute('src', imageData.url!)
+  document.getElementById('imageTitle')!.textContent = imageData.title!
+  document.getElementById('imageDate')!.textContent = imageData.date!
+  document.getElementById('imageDescription')!.textContent =
+    imageData.explanation!
 }
 
-new Cards(handleImageClick);
+new Cards(handleImageClick)
 
-// Function to fetch daily image
 function fetchDailyImage(dateToFetch: string) {
-	const url = `https://api.nasa.gov/planetary/apod?date=${dateToFetch}&api_key=E1RwjszVbe0bxmJHHZ5mUr8uDuTpKUYPiHkTVosB`;
+  const url = `https://api.nasa.gov/planetary/apod?date=${dateToFetch}&api_key=E1RwjszVbe0bxmJHHZ5mUr8uDuTpKUYPiHkTVosB`
 
-	fetch(url)
-		.then((response) => response.json())
-		.then((data) => {
-			document.getElementById("dailyImage")!.setAttribute("src", data.url!);
-			document.getElementById("imageTitle")!.textContent = data.title!;
-			document.getElementById("imageDate")!.textContent = data.date!;
-			document.getElementById("imageDescription")!.textContent =
-				data.explanation!;
-		})
-		.catch((error) => console.error("Error fetching daily image:", error));
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById('dailyImage')!.setAttribute('src', data.url!)
+      document.getElementById('imageTitle')!.textContent = data.title!
+      document.getElementById('imageDate')!.textContent = data.date!
+      document.getElementById('imageDescription')!.textContent =
+        data.explanation!
+    })
+    .catch((error) => console.error('Error fetching daily image:', error))
 }
 
-// Attach an event listener to the search button
-document.getElementById("searchButton")!.addEventListener("click", () => {
-	const selectedDate = (
-		document.getElementById("searchDate") as HTMLInputElement
-	).value;
-	fetchDailyImage(selectedDate);
-});
+document.getElementById('searchButton')!.addEventListener('click', () => {
+  const selectedDate = (
+    document.getElementById('searchDate') as HTMLInputElement
+  ).value
+  fetchDailyImage(selectedDate)
+})
 
-// Fetch the image for today on initial load
-fetchDailyImage(new Date().toISOString().split("T")[0]);
+fetchDailyImage(new Date().toISOString().split('T')[0])
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAoARK8yJAehauyYbcx3nMkI6u9jSuUhms',
+  authDomain: 'astronomy-pictures-fe269.firebaseapp.com',
+  projectId: 'astronomy-pictures-fe269',
+  storageBucket: 'astronomy-pictures-fe269.appspot.com',
+  messagingSenderId: '852557719622',
+  appId: '1:852557719622:web:3c96fcbc99ffd5019e0247',
+  measurementId: 'G-2YWDEK86BX',
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
