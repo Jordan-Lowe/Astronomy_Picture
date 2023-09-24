7539d43220f29a9be07b63fa3b450216177b0ce6
Jordan Lowe <jordanlowe659@gmail.com>
2023-09-25 12:51:35 +1300

    Fucked

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ import {
   increment,
   setDoc,
 } from 'firebase/firestore'
-
 import {
   fetchRandomImages,
   fetchPopularImages,
@@ -30,7 +29,6 @@ interface Data {
 
 class Cards {
   private photos: Data[] = []
-  private cardState: string = 'photos'
   private fetchDataFunction: () => Promise<Data[] | Data>
 
   constructor(
@@ -41,20 +39,54 @@ class Cards {
     this.fetchData()
   }
 
+  clear() {
+    const imageWrapper = document.getElementById('imageWrapper')
+    if (imageWrapper) {
+      imageWrapper.innerHTML = ''
+    }
+  }
+
+  updateFetchDataFunction(newFetchDataFunction: () => Promise<Data[] | Data>) {
+    this.fetchDataFunction = newFetchDataFunction
+  }
+
+  async fetchData() {
+    try {
+      const data = await this.fetchDataFunction()
+      if (Array.isArray(data)) {
+        this.photos = data
+      } else {
+        this.photos = [data]
+      }
+      this.photos.forEach(this.storeDataInFirebase)
+      this.render()
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+
+  render() {
+    const imageWrapper = document.getElementById('imageWrapper')
+    if (imageWrapper) {
+      this.photos.forEach((photo) => {
+        const imgElement = document.createElement('img')
+        imgElement.src = photo.url!
+        imageWrapper.appendChild(imgElement)
+      })
+    }
+  }
+
   private async storeDataInFirebase(data: Data) {
     if (data.date) {
       try {
         const docRef = doc(db, 'nasaData', data.date)
         const docSnap = await getDoc(docRef)
-
         if (!docSnap.exists()) {
-          // If document doesn't exist, create it with clicks set to 0
           await setDoc(docRef, {
             ...data,
             clicks: 0,
           })
         }
-        // If the document already exists, we don't need to do anything here since the clicks will be updated via the incrementClickCount method
       } catch (error) {
         console.error('Error checking or adding document:', error)
       }
@@ -66,87 +98,64 @@ class Cards {
       try {
         const docRef = doc(db, 'nasaData', data.date)
         const docSnap = await getDoc(docRef)
-
         if (docSnap.exists()) {
-          // If document exists, update the clicks
           await updateDoc(docRef, {
             clicks: increment(1),
           })
         } else {
-          // If document doesn't exist, create it with clicks set to 1
           await setDoc(docRef, {
             ...data,
             clicks: 1,
           })
         }
       } catch (error) {
-        console.error('Error updating click count:', error)
+        console.error('Error incrementing click count:', error)
       }
     }
   }
-
-  private fetchData() {
-    fetch(
-      'https://api.nasa.gov/planetary/apod?api_key=E1RwjszVbe0bxmJHHZ5mUr8uDuTpKUYPiHkTVosB&count=2 '
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        this.photos = data
-        this.photos.forEach((photo) => {
-          this.storeDataInFirebase(photo)
-        })
-        this.render()
-      })
-      .catch((error) => console.error('Error fetching photos:', error))
-  }
-
-  public render() {
-    const cardWrapper = document.createElement('div')
-    cardWrapper.className = 'cardWrapper'
-    cardWrapper.setAttribute('data-state', this.cardState)
-
-    const uiLayer = document.createElement('div')
-    uiLayer.className = 'ui-layer'
-
-    this.photos.forEach((photoData) => {
-      const uiPhoto = document.createElement('div')
-      uiPhoto.className = 'ui-photo'
-      uiPhoto.setAttribute('data-photo', photoData.url!)
-      uiPhoto.addEventListener('click', () => {
-        this.onImageClick(photoData)
-        this.incrementClickCount(photoData)
-      })
-
-      const img = document.createElement('img')
-      img.src = photoData.url!
-      img.alt = photoData.title!
-
-      uiPhoto.appendChild(img)
-      uiLayer.appendChild(uiPhoto)
-    })
-
-    cardWrapper.appendChild(uiLayer)
-
-    const container = document.querySelector('#leftContainer')
-    container?.appendChild(cardWrapper)
-  }
-
-  public clear() {
-    const cardWrappers = document.querySelectorAll('.cardWrapper')
-    cardWrappers.forEach((wrapper) => wrapper.remove())
-  }
 }
 
-function handleImageClick(imageData: Data): void {
-  document.getElementById('dailyImage')!.setAttribute('src', imageData.url!)
-  document.getElementById('imageTitle')!.textContent = imageData.title!
-  document.getElementById('imageDate')!.textContent = imageData.date!
-  document.getElementById('imageDescription')!.textContent =
-    imageData.explanation!
+const firebaseConfig = {
+  apiKey: 'AIzaSyAoARK8yJAehauyYbcx3nMkI6u9jSuUhms',
+  authDomain: 'astronomy-pictures-fe269.firebaseapp.com',
+  projectId: 'astronomy-pictures-fe269',
+  storageBucket: 'astronomy-pictures-fe269.appspot.com',
+  messagingSenderId: '852557719622',
+  appId: '1:852557719622:web:3c96fcbc99ffd5019e0247',
+  measurementId: 'G-2YWDEK86BX',
 }
 
-new Cards(handleImageClick, fetchRandomImages)
-fetchDailyImage(new Date().toISOString().split('T')[0])
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+const cardsInstance = new Cards(handleImageClick, fetchRandomImages)
+
+const tabSwitch = document.querySelectorAll('.tabItems')
+tabSwitch.forEach((tab) => {
+  tab.addEventListener('click', async (e) => {
+    cardsInstance.clear()
+    const tabName = (e.target as HTMLElement).textContent
+    switch (tabName) {
+      case 'Random':
+        cardsInstance.updateFetchDataFunction(fetchRandomImages)
+        break
+      case 'Popular':
+        cardsInstance.updateFetchDataFunction(fetchPopularImages)
+        break
+      case 'Latest':
+        cardsInstance.updateFetchDataFunction(fetchLatestImage)
+        break
+    }
+    await cardsInstance.fetchData()
+  })
+})
+
+document.getElementById('searchButton')!.addEventListener('click', () => {
+  const selectedDate = (
+    document.getElementById('searchDate') as HTMLInputElement
+  ).value
+  fetchDailyImage(selectedDate)
+})
 
 function fetchDailyImage(dateToFetch: string) {
   const url = `https://api.nasa.gov/planetary/apod?date=${dateToFetch}&api_key=E1RwjszVbe0bxmJHHZ5mUr8uDuTpKUYPiHkTVosB`
@@ -163,48 +172,9 @@ function fetchDailyImage(dateToFetch: string) {
     .catch((error) => console.error('Error fetching daily image:', error))
 }
 
-document.getElementById('searchButton')!.addEventListener('click', () => {
-  const selectedDate = (
-    document.getElementById('searchDate') as HTMLInputElement
-  ).value
-  fetchDailyImage(selectedDate)
-})
-
 fetchDailyImage(new Date().toISOString().split('T')[0])
-
-//Tab switch functionality
-// Tab switch functionality
-const tabSwitch = document.querySelectorAll('.tabItems')
-tabSwitch.forEach((tab) => {
-  tab.addEventListener('click', async (e) => {
-    // Clear previous images
-    const cardsInstance = new Cards(handleImageClick, fetchRandomImages)
-    cardsInstance.clear()
-
-    const tabName = (e.target as HTMLElement).textContent
-    switch (tabName) {
-      case 'Random':
-        new Cards(handleImageClick, fetchRandomImages)
-        break
-      case 'Popular':
-        new Cards(handleImageClick, fetchPopularImages)
-        break
-      case 'Latest':
-        new Cards(handleImageClick, fetchLatestImage)
-        break
-    }
-  })
-})
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyAoARK8yJAehauyYbcx3nMkI6u9jSuUhms',
-  authDomain: 'astronomy-pictures-fe269.firebaseapp.com',
-  projectId: 'astronomy-pictures-fe269',
-  storageBucket: 'astronomy-pictures-fe269.appspot.com',
-  messagingSenderId: '852557719622',
-  appId: '1:852557719622:web:3c96fcbc99ffd5019e0247',
-  measurementId: 'G-2YWDEK86BX',
+function handleImageClick(imageData: Data): void {
+  throw new Error('Function not implemented.')
 }
 
-const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
+initializeApp(firebaseConfig)
