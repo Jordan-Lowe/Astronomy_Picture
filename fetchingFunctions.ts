c222054ab2f545d856aa6094d982ef130ed168bd
Jordan-Lowe <jordanlowe659@gmail.com>
2023-10-21 09:53:13 +1300

    sstylingChanges

--- fetchingFunctions.ts
+++ fetchingFunctions.ts
@@ -1,156 +1,156 @@
-import { Data } from './data'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import { Data } from "./data";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app";
 import {
-  getFirestore,
-  collection,
-  getDoc,
-  doc,
-  updateDoc,
-  increment,
-  setDoc,
-  getDocs,
-  limit,
-  orderBy,
-  query,
-} from 'firebase/firestore'
+	getFirestore,
+	collection,
+	getDoc,
+	doc,
+	updateDoc,
+	increment,
+	setDoc,
+	getDocs,
+	limit,
+	orderBy,
+	query,
+} from "firebase/firestore";
 
-const API_KEY = process.env.NASA_API_KEY!
-const NASA_API_BASE = 'https://api.nasa.gov/planetary/apod'
+const API_KEY = process.env.NASA_API_KEY!;
+const NASA_API_BASE = "https://api.nasa.gov/planetary/apod";
 
 // Fetch random images from NASA API
 
 export async function fetchRandomImages(): Promise<Data[]> {
-  // Construct API URL
-  const url = `${NASA_API_BASE}?count=9&api_key=${API_KEY}`
+	// Construct API URL
+	const url = `${NASA_API_BASE}?count=9&api_key=${API_KEY}`;
 
-  // Fetch data from API
-  const response = await fetch(url)
+	// Fetch data from API
+	const response = await fetch(url);
 
-  // Parse response as JSON
-  const data: Data[] = await response.json()
+	// Parse response as JSON
+	const data: Data[] = await response.json();
 
-  // Store data in Firestore
-  data.forEach((image) => {
-    storeDataInFirebase(image)
-  })
+	// Store data in Firestore
+	data.forEach((image) => {
+		storeDataInFirebase(image);
+	});
 
-  // Return fetched data
-  return data
+	// Return fetched data
+	return data;
 }
 
 // Fetch popular images from Firestore
 export async function fetchPopularImages(): Promise<Data[]> {
-  // Define query to get 2 most popular images
-  const q = query(
-    collection(db, 'nasaData'),
-    orderBy('clicks', 'desc'),
-    limit(9)
-  )
+	// Define query to get 2 most popular images
+	const q = query(
+		collection(db, "nasaData"),
+		orderBy("clicks", "desc"),
+		limit(9)
+	);
 
-  // Get query snapshot from Firestore
-  const querySnapshot = await getDocs(q)
+	// Get query snapshot from Firestore
+	const querySnapshot = await getDocs(q);
 
-  // Initialize array to store popular images
-  const popularImages: Data[] = []
+	// Initialize array to store popular images
+	const popularImages: Data[] = [];
 
-  // Loop through query snapshot and push data to popularImages array
-  querySnapshot.forEach((doc) => {
-    popularImages.push(doc.data() as Data)
-  })
+	// Loop through query snapshot and push data to popularImages array
+	querySnapshot.forEach((doc) => {
+		popularImages.push(doc.data() as Data);
+	});
 
-  // Return popular images array
-  return popularImages
+	// Return popular images array
+	return popularImages;
 }
 
 export async function fetchLastImages(): Promise<Data[]> {
-  const latestImages: Data[] = []
-  for (let i = 0; i < 9; i++) {
-    const date = new Date()
-    date.setDate(date.getDate() - i)
-    const dateString = date.toISOString().split('T')[0] // format as YYYY-MM-DD
-    const url = `${NASA_API_BASE}?date=${dateString}&api_key=${API_KEY}`
-    const response = await fetch(url)
-    if (response.ok) {
-      const data: Data = await response.json()
-      latestImages.push(data)
-    }
-  }
-  return latestImages
+	const latestImages: Data[] = [];
+	for (let i = 0; i < 9; i++) {
+		const date = new Date();
+		date.setDate(date.getDate() - i);
+		const dateString = date.toISOString().split("T")[0]; // format as YYYY-MM-DD
+		const url = `${NASA_API_BASE}?date=${dateString}&api_key=${API_KEY}`;
+		const response = await fetch(url);
+		if (response.ok) {
+			const data: Data = await response.json();
+			latestImages.push(data);
+		}
+	}
+	return latestImages;
 }
 
 export async function fetchLatestImage(): Promise<Data> {
-  const today = new Date().toISOString().split('T')[0]
-  const url = `${NASA_API_BASE}?date=${today}&api_key=${API_KEY}`
-  const response = await fetch(url)
-  const data: Data = await response.json()
-  return data
+	const today = new Date().toISOString().split("T")[0];
+	const url = `${NASA_API_BASE}?date=${today}&api_key=${API_KEY}`;
+	const response = await fetch(url);
+	const data: Data = await response.json();
+	return data;
 }
 
 export async function fetchDailyImage(dateToFetch: string) {
-  const url = `${NASA_API_BASE}?date=${dateToFetch}&api_key=${API_KEY}`
+	const url = `${NASA_API_BASE}?date=${dateToFetch}&api_key=${API_KEY}`;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      document.getElementById('dailyImage')!.setAttribute('src', data.url!)
-      document.getElementById('imageTitle')!.textContent = data.title!
-      document.getElementById('imageDate')!.textContent = data.date!
-      document.getElementById('imageDescription')!.textContent =
-        data.explanation!
-    })
-    .catch((error) => console.error('Error fetching daily image:', error))
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			document.getElementById("dailyImage")!.setAttribute("src", data.url!);
+			document.getElementById("imageTitle")!.textContent = data.title!;
+			document.getElementById("imageDate")!.textContent = data.date!;
+			document.getElementById("imageDescription")!.textContent =
+				data.explanation!;
+		})
+		.catch((error) => console.error("Error fetching daily image:", error));
 }
 
 export async function incrementClickCount(data: Data) {
-  if (data.date) {
-    try {
-      const docRef = doc(db, 'nasaData', data.date)
-      const docSnap = await getDoc(docRef)
-      if (docSnap.exists()) {
-        await updateDoc(docRef, {
-          clicks: increment(1),
-        })
-      } else {
-        await setDoc(docRef, {
-          ...data,
-          clicks: 1,
-        })
-      }
-    } catch (error) {
-      console.error('Error incrementing click count:', error)
-    }
-  }
+	if (data.date) {
+		try {
+			const docRef = doc(db, "nasaData", data.date);
+			const docSnap = await getDoc(docRef);
+			if (docSnap.exists()) {
+				await updateDoc(docRef, {
+					clicks: increment(1),
+				});
+			} else {
+				await setDoc(docRef, {
+					...data,
+					clicks: 1,
+				});
+			}
+		} catch (error) {
+			console.error("Error incrementing click count:", error);
+		}
+	}
 }
 
 async function storeDataInFirebase(data: Data) {
-  if (data.date) {
-    try {
-      const docRef = doc(db, 'nasaData', data.date)
-      const docSnap = await getDoc(docRef)
-      if (!docSnap.exists()) {
-        await setDoc(docRef, {
-          ...data,
-          clicks: 0,
-        })
-      }
-    } catch (error) {
-      console.error('Error checking or adding document:', error)
-    }
-  }
+	if (data.date) {
+		try {
+			const docRef = doc(db, "nasaData", data.date);
+			const docSnap = await getDoc(docRef);
+			if (!docSnap.exists()) {
+				await setDoc(docRef, {
+					...data,
+					clicks: 0,
+				});
+			}
+		} catch (error) {
+			console.error("Error checking or adding document:", error);
+		}
+	}
 }
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY!,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.FIREBASE_APP_ID!,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID!,
-}
+	apiKey: process.env.FIREBASE_API_KEY!,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+	projectId: process.env.FIREBASE_PROJECT_ID!,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+	appId: process.env.FIREBASE_APP_ID!,
+	measurementId: process.env.FIREBASE_MEASUREMENT_ID!,
+};
 
-const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
--- main.ts
+++ main.ts
@@ -1,140 +1,138 @@
-import { Data } from './data'
-import { DateTime } from 'luxon'
+import { Data } from "./data";
+import { DateTime } from "luxon";
 
 import {
-  fetchRandomImages,
-  fetchPopularImages,
-  fetchLatestImage,
-  fetchDailyImage,
-  incrementClickCount,
-  fetchLastImages,
-} from './fetchingFunctions'
+	fetchRandomImages,
+	fetchPopularImages,
+	fetchLatestImage,
+	fetchDailyImage,
+	incrementClickCount,
+	fetchLastImages,
+} from "./fetchingFunctions";
 
-const randomTab = document.getElementById('random')
-const popularTab = document.getElementById('popular')
-const latestTab = document.getElementById('latest')
+const randomTab = document.getElementById("random");
+const popularTab = document.getElementById("popular");
+const latestTab = document.getElementById("latest");
 
 //Observer for lazy loading
 const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const img = entry.target as HTMLImageElement
-        img.src = img.getAttribute('data-src') || ''
-        observer.unobserve(img)
-      }
-    })
-  },
-  { threshold: 0.01 }
-)
+	(entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				const img = entry.target as HTMLImageElement;
+				img.src = img.getAttribute("data-src") || "";
+				observer.unobserve(img);
+			}
+		});
+	},
+	{ threshold: 0.01 }
+);
 
 // Observe images with 'data-src' attribute
 document
-  .querySelectorAll('img[data-src]')
-  .forEach((img) => observer.observe(img as Element))
+	.querySelectorAll("img[data-src]")
+	.forEach((img) => observer.observe(img as Element));
 
 // Observe images with 'data-src' attribute
 document
-  .querySelectorAll('img[data-src]')
-  .forEach((img) => observer.observe(img as Element))
+	.querySelectorAll("img[data-src]")
+	.forEach((img) => observer.observe(img as Element));
 
 document
-  .querySelectorAll('img[data-src]')
-  .forEach((img) => observer.observe(img as Element))
+	.querySelectorAll("img[data-src]")
+	.forEach((img) => observer.observe(img as Element));
 
 // Set up event listeners on the list items
-randomTab?.addEventListener('click', async () => {
-  const images = await fetchRandomImages()
-  displayImages(images)
-})
+randomTab?.addEventListener("click", async () => {
+	const images = await fetchRandomImages();
+	displayImages(images);
+});
 
-popularTab?.addEventListener('click', async () => {
-  // Fetch the latest image from the server
-  const images = await fetchPopularImages()
-  displayImages(images)
-})
+popularTab?.addEventListener("click", async () => {
+	// Fetch the latest image from the server
+	const images = await fetchPopularImages();
+	displayImages(images);
+});
 
-latestTab?.addEventListener('click', async () => {
-  const images = await fetchLastImages() // changed from fetchLatestImage to fetchLastImages
-  displayImages(images)
-})
+latestTab?.addEventListener("click", async () => {
+	const images = await fetchLastImages(); // changed from fetchLatestImage to fetchLastImages
+	displayImages(images);
+});
 
 function displayImages(images: Data[]) {
-  for (let i = 0; i < 9; i++) {
-    const photo = document.getElementById(`photo${i + 1}`) as HTMLImageElement
-    if (images[i]) {
-      photo.setAttribute('data-src', images[i].url ?? '')
-      photo.alt = images[i].title ?? ''
-      observer.observe(photo) // Ensure the observer is observing the image
+	for (let i = 0; i < 9; i++) {
+		const photo = document.getElementById(`photo${i + 1}`) as HTMLImageElement;
+		if (images[i]) {
+			photo.setAttribute("data-src", images[i].url ?? "");
+			photo.alt = images[i].title ?? "";
+			observer.observe(photo); // Ensure the observer is observing the image
 
-      photo.onload = function () {
-        ;(this as HTMLImageElement).classList.add('loaded') // Add 'loaded' class to the image when it's loaded
-      }
-    }
-  }
+			photo.onload = function () {
+				(this as HTMLImageElement).classList.add("loaded"); // Add 'loaded' class to the image when it's loaded
+			};
+		}
+	}
 }
 
 function attachClickEventToPhotos(images: Data[]) {
-  for (let i = 0; i < images.length; i++) {
-    const photoDiv = document.querySelector(`.ui-photos:nth-child(${i + 1})`)
-    if (photoDiv) {
-      photoDiv.addEventListener('click', () => {
-        updateLeftWrapper(images[i])
-        incrementClickCount(images[i])
-      })
-    }
-  }
+	for (let i = 0; i < images.length; i++) {
+		const photoDiv = document.querySelector(`.ui-photos:nth-child(${i + 1})`);
+		if (photoDiv) {
+			photoDiv.addEventListener("click", () => {
+				updateLeftWrapper(images[i]);
+				incrementClickCount(images[i]);
+			});
+		}
+	}
 }
 
 function updateLeftWrapper(image: Data) {
-  const dailyImage = document.getElementById('dailyImage') as HTMLImageElement
-  const imageTitle = document.getElementById('imageTitle') as HTMLElement
-  const imageDate = document.getElementById('imageDate') as HTMLElement
-  const imageDescription = document.getElementById(
-    'imageDescription'
-  ) as HTMLElement
+	const dailyImage = document.getElementById("dailyImage") as HTMLImageElement;
+	const imageTitle = document.getElementById("imageTitle") as HTMLElement;
+	const imageDate = document.getElementById("imageDate") as HTMLElement;
+	const imageDescription = document.getElementById(
+		"imageDescription"
+	) as HTMLElement;
 
-  dailyImage.src = image.url ?? ''
-  imageTitle.textContent = image.title ?? ''
-  imageDate.textContent = image.date ?? ''
-  imageDescription.textContent = image.explanation ?? ''
+	dailyImage.src = image.url ?? "";
+	imageTitle.textContent = image.title ?? "";
+	imageDate.textContent = image.date ?? "";
+	imageDescription.textContent = image.explanation ?? "";
 }
 
 // After fetching and displaying images, attach click event to each .ui-photos div.
 async function onTabClick(fetchImages: () => Promise<Data[]>) {
-  const images = await fetchImages()
-  displayImages(images)
-  attachClickEventToPhotos(images)
+	const images = await fetchImages();
+	displayImages(images);
+	attachClickEventToPhotos(images);
 }
 
-randomTab?.addEventListener('click', () => onTabClick(fetchRandomImages))
-popularTab?.addEventListener('click', () => onTabClick(fetchPopularImages))
-latestTab?.addEventListener('click', async () => {
-  const image = await fetchLatestImage()
-  displayImages([image])
-  attachClickEventToPhotos([image])
-})
+randomTab?.addEventListener("click", () => onTabClick(fetchRandomImages));
+popularTab?.addEventListener("click", () => onTabClick(fetchPopularImages));
+latestTab?.addEventListener("click", async () => {
+	const image = await fetchLatestImage();
+	displayImages([image]);
+	attachClickEventToPhotos([image]);
+});
 
-document.getElementById('searchButton')!.addEventListener('click', () => {
-  const selectedDate = (
-    document.getElementById('searchDate') as HTMLInputElement
-  ).value
-  fetchDailyImage(selectedDate)
-})
+document.getElementById("searchButton")!.addEventListener("click", () => {
+	const selectedDate = (
+		document.getElementById("searchDate") as HTMLInputElement
+	).value;
+	fetchDailyImage(selectedDate);
+});
 
-fetchDailyImage(new Date().toISOString().split('T')[0])
+fetchDailyImage(new Date().toISOString().split("T")[0]);
 
-window.addEventListener('load', async () => {
-  const images = await fetchRandomImages()
-  displayImages(images)
-  attachClickEventToPhotos(images)
+window.addEventListener("load", async () => {
+	const images = await fetchRandomImages();
+	displayImages(images);
+	attachClickEventToPhotos(images);
 
-  const todayInET = DateTime.local().setZone('America/New_York')
-  const today = todayInET.toISODate()
+	const todayInET = DateTime.local().setZone("America/New_York");
+	const today = todayInET.toISODate();
 
-  if (today !== null) {
-    fetchDailyImage(today)
-  }
-})
-
-console.log('Commit works')
+	if (today !== null) {
+		fetchDailyImage(today);
+	}
+});
